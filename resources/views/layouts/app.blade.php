524b3bf9705d45fc9bf5f5f2f69b0485675f5168
anhnhat1506 <nhat15061992@gmail.com>
2021-04-06 16:31:20 +0700

    auth

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,18 +20,23 @@
         </ul>
 
         <ul class="flex items-center">
-            <li>
-                <a href="" class="p-3">Revival</a>
-            </li>
-            <li>
-                <a href="" class="p-3">Login</a>
-            </li>
-            <li>
-                <a href="" class="p-3">Register</a>
-            </li>
-            <li>
-                <a href="" class="p-3">Logout</a>
-            </li>
+            @auth
+                <li>
+                    <a href="" class="p-3">Revival</a>
+                </li>
+                <li>
+                    <a href="" class="p-3">Login</a>
+                </li>
+            @endauth
+
+            @guest
+                <li>
+                    <a href="{{ route('register') }}" class="p-3">Register</a>
+                </li>
+                <li>
+                    <a href="" class="p-3">Logout</a>
+                </li>
+            @endguest
         </ul>
     </nav>
     @yield('content')
